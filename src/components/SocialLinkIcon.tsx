ff26c06d6c711c93c4c2a9d005dbe35a02373af4
s4kr4 <s4kr4m4@gmail.com>
2025-03-24 00:33:33 +0900

    Add SocialLinkIcon component

---
+++ src/components/SocialLinkIcon.tsx
@@ -0,0 +1,37 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styles from "./SocialLinkIcon.module.css";
+
+type SocialLinkIconProps = {
+  className?: string
+  name: IconProp
+  text: string
+  url: string
+  modifier: string
+}
+
+function SocialLinkIcon({ 
+  className,
+  name,
+  text,
+  url,
+  modifier,
+ }: SocialLinkIconProps) {
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`${className || ''} ${styles.container}`}
+      aria-label={text}
+    >
+      <FontAwesomeIcon
+        icon={name}
+        className={styles[modifier]}
+        size="4x"
+      />
+    </a>
+  );
+}
+
+export default SocialLinkIcon;
--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,56 +1,65 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import SocialLinkIcon from "@/components/SocialLinkIcon";
 import styles from "./SocialLinks.module.css";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faGithub, faQuora, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+
+type Site = {
+  icon: IconProp
+  text: string
+  url: string
+  option: string
+  modifier: string
+}
+
+const sites: Array<Site> = [
+  {
+    icon: faGithub,
+    text: 'GitHub',
+    url: 'https://github.com/s4kr4',
+    option: 'spin',
+    modifier: 'github-icon',
+  },
+  {
+    icon: faQuora,
+    text: 'Qiita',
+    url: 'https://qiita.com/s4kr4',
+    option: 'vertical',
+    modifier: 'qiita-icon',
+  },
+  {
+    icon: faEdit,
+    text: 'Blog',
+    url: 'http://s4kr4.hatenablog.com/',
+    option: 'vertical',
+    modifier: 'hatenablog-icon',
+  },
+  {
+    icon: faTwitter,
+    text: 'Twitter',
+    url: 'https://twitter.com/s4kr4_',
+    option: 'vertical',
+    modifier: 'twitter-icon',
+  },
+]
 
 function SocialLinks() {
+  const siteIcons = sites.map((site, i) => {
+    return (
+      <SocialLinkIcon
+        className="menu-item"
+        key={i}
+        name={site.icon}
+        text={site.text}
+        url={site.url}
+        modifier={site.modifier}
+      />
+    )
+  })
+
   return (
     <div className={styles.container}>
-      <a
-        href="https://github.com/s4kr4"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.link}
-      >
-        <FontAwesomeIcon
-          icon={faGithub}
-          className={styles.whiteIcon}
-          size="2x"
-        />
-      </a>
-      <a
-        href="https://qiita.com/s4kr4"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.link}
-      >
-        <span className={styles.qiitaText}>Q</span>
-      </a>
-      <a
-        href="https://zenn.dev/s4kr4"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.link}
-      >
-        <FontAwesomeIcon
-          icon={faPenToSquare}
-          className={styles.whiteIcon}
-          size="2x"
-        />
-      </a>
-      <a
-        href="https://twitter.com/s4kr4_"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.link}
-      >
-        <FontAwesomeIcon
-          icon={faTwitter}
-          className={styles.twitterIcon}
-          size="2x"
-        />
-      </a>
+      {siteIcons}
     </div>
   );
 }
