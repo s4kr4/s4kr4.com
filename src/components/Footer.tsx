dd5327ad0c531ad5e84fd1f35b8c43b4f4286f91
s4kr4 <s4kr4m4@gmail.com>
2025-03-23 23:34:15 +0900

    Fix copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="py-4 text-center border-t border-gray-800">
-      © 2021
+      © {year} s4kr4
     </footer>
   );
 }
