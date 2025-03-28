bd1e834b2dae095e2cc8d841a43e696219603927
s4kr4 <s4kr4m4@gmail.com>
2025-03-29 03:00:09 +0900

    Update styles

--- src/app/works/_components/WorkItem.tsx
+++ src/app/works/_components/WorkItem.tsx
@@ -2,6 +2,7 @@ import { Work } from "@/app/works/page";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./WorkItem.module.css";
 
 type WorkItemProps = {
   work: Work;
@@ -11,9 +12,9 @@ function WorkItem({ work }: WorkItemProps) {
   const { title, imageSrc, description, source } = work;
 
   return (
-    <Card>
+    <Card className={styles.container}>
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
+        <CardTitle className={styles.title}>{title}</CardTitle>
       </CardHeader>
       <CardContent>
         <Image
